c747fbad06b689b75a3d144a23c474e1c29f2c08
Shudhanshu_Shrotriya <75602183+ShudhanshuGR8@users.noreply.github.com>
2022-08-04 00:49:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from flask import Flask , render_template , request, jsonify
 from flask_cors import CORS
 from chat import get_response
 
-
-
-app = Flask ( __name__ )
-CORS(app)
-
+@app.get("/")
+def index_get():
+    return render_template("base.html")
 
 
 
