548c24065abccdb40b4ce1f46081037e20755e80
Shudhanshu_Shrotriya <75602183+ShudhanshuGR8@users.noreply.github.com>
2022-08-04 00:16:03 +0530

    Update train.py

--- train.py
+++ train.py
@@ -8,6 +8,9 @@ from torch.utils.data import Dataset, DataLoader
 
 from nltk_utils import bag_of_words, tokenize, stem
 from model import NeuralNet
+import nltk
+nltk.download('punkt')
+
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
